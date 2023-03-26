3f31a234fcf5f6460ff154e6e9400a7e9b753763
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-03-26 09:49:21 +0530

    Create Single Number.java

---
+++ Single Number.java
@@ -0,0 +1,16 @@
+
+
+//Time Complexity - O(n)
+//Auxilary Space - O(1)
+
+
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res=0;
+        for(int i=0;i<nums.length;i++){
+            res=res^nums[i];
+        }
+        return res;
+    }
+}

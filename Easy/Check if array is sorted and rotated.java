6d22eb813363edfa866c8bf4af317da206edbded
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-04-16 11:27:41 +0530

    Create Check if array is sorted and rotated.java

---
+++ Easy/Check if array is sorted and rotated.java
@@ -0,0 +1,21 @@
+
+
+//Time Complexity - O(n)
+//Auxilary Space - O(1)
+
+
+class Solution {
+    public boolean check(int[] nums) {
+        int n=nums.length;
+        int count=0;
+        for(int i=1;i<n;i++){
+            if(nums[i]<nums[i-1]){
+                count++;
+            }
+        }
+        if(nums[n-1]>nums[0]){
+            count++;
+        }
+        return count<=1;
+    }
+}

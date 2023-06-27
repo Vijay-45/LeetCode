1f5c3686716837d47d3060d4c7ff1fce33243427
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-06-27 11:31:58 +0530

    Update Group Anagrams.java

--- Medium/Group Anagrams.java
+++ Medium/Group Anagrams.java
@@ -41,3 +41,33 @@ class Solution {
         return true;
     }
 }
+
+
+_____________________________________________________________________________________________________________________________________________________________________________
+
+
+
+
+//Time Complexity - O(n*klogk)
+//Auxilary Space - O(n*k)
+
+
+
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] arr) {
+        if(arr==null || arr.length==0) return new ArrayList<>();
+        Map<String,List<String>> map=new HashMap<>();
+        for(String s:arr){
+            char[] arr1=s.toCharArray();
+            Arrays.sort(arr1);
+            String res=String.valueOf(arr1);
+            if(!map.containsKey(res)){
+                map.put(res,new ArrayList<>());
+            }
+            map.get(res).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+    
+}

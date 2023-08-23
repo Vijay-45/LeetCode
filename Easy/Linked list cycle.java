fdc879c512a948bfc280a914357e85732ed1395d
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-08-23 10:38:48 +0530

    Create Linked list cycle.java

---
+++ Easy/Linked list cycle.java
@@ -0,0 +1,20 @@
+
+
+//Time Complexity - O(n)
+//Auxilary Space - O(1)
+
+
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast=head,slow=head;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}

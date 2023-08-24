1f57919bb2b4aa04182325e3a55dfa204d33f43e
Vijay Kumar <110212936+Vijay-45@users.noreply.github.com>
2023-08-24 14:06:38 +0530

    Create Remove Duplicates from sorted list.java

---
+++ Easy/Remove Duplicates from sorted list.java
@@ -0,0 +1,23 @@
+
+
+//Time Complexity - O(n)
+//Auxilary Space - O(1)
+
+
+
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode curr=head;
+        if(head==null) return head;
+        while(curr.next!=null){
+            if(curr.val==curr.next.val){
+                curr.next=curr.next.next;
+            }
+            else{
+                curr=curr.next;
+            }
+        }
+        return head;
+    }
+}
